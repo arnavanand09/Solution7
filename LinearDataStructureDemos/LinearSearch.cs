648e9460c14e6a17993e27970a1b94855e164f04
Anamika Sawhney <anamika.sawhney22@gmail.com>
2025-07-30 12:42:10 +0530

    sorting demos

---
+++ LinearDataStructureDemos/LinearSearch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearDataStructureDemos
+{
+    enum Search {Linear=1, Binary=2 };
+    internal class SearchDemos
+    {
+        static int[] num = [2, 3, 13, 14, 45, 6, 56, 76, 3];
+        static void Main()
+        {
+            DisplayElements();
+            Console.WriteLine("enter number to search");
+            int data = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Which Search algo ");
+            int ch = byte.Parse(Console.ReadLine());
+            bool found = false;
+            switch (ch)
+            {
+                case (int)Search.Linear:
+                    {
+                          found = LinearSearch(data); break;
+                    }
+                case (int)Search.Binary:
+
+                    {  Array.Sort(num);
+                      found = BinarySearch(data); break; }
+            }
+            Console.WriteLine(found == true ? "Element found" : "Element not found");
+
+        }
+        static bool LinearSearch(int data)
+        {
+            bool flag = false;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == data)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+        }
+        static bool BinarySearch(int data)
+        {
+            bool flag = false;
+            int low = 0 , high = num.Length - 1, mid = (low+high)/2; 
+            while(low <= high) {
+
+                if(data == num[mid])
+                { 
+                     flag = true;
+                    break;
+                }
+                else if(data < num[mid])
+                {
+                    high = mid+1;
+                }
+                else if(data > num[mid])
+                {
+                    low = mid + 1;
+                }
+                mid = (low+high)/2;
+            }
+            return flag;
+        }
+
+        static void DisplayElements()
+        {
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ LinearDataStructureDemos/SortingDemos.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearDataStructureDemos
+{
+    enum Sort { Bubble=1, Selection, Insertion, Shell };
+
+    internal class SortingDemos
+    {
+        static int[] num = [12, 23, 34, 11, 35, 11, 9, 5, 6, 3, 1, 10];
+        static void Main()
+        {
+            DisplayElements();
+            Console.WriteLine("Which sorting tech");
+            int ch = byte.Parse(Console.ReadLine());
+            switch (ch)
+            {
+                case (int)Sort.Bubble: BubbleSort(); break;
+                case (int)Sort.Selection: SelectionSort(); break;
+                case (int)Sort.Shell: ShellSort(); break;
+
+            }
+            DisplayElements();
+        }
+        static void BubbleSort()
+        {
+            int temp;
+            for (int i = 0; i <= num.Length - 2; i++)
+            {
+                for (int j = 0; j <= num.Length - 2; j++)
+                {
+                    if (num[j] > num[j + 1])
+                    {
+                        temp = num[j + 1];
+                        num[j + 1] = num[j];
+                        num[j] = temp;
+                    }
+                }
+
+            }
+        }
+
+        static void SelectionSort()
+        {
+            int temp;
+            for (int i = 0; i < num.Length-1; i++)
+            {
+                for (int j = i+1; j < num.Length; j++)
+                {
+                    if (num[i] > num[j])
+                    {
+                        temp = num[j];
+                        num[j] = num[i];
+                        num[i] = temp;
+                    }
+                }
+
+            }
+        }
+
+        static void ShellSort()
+        {
+            int n = num.Length;
+            int gap = n / 2;
+            int temp;
+            while (gap > 0)
+            {
+                for (int i = 0; i + gap < n; i++)
+                {
+                    int j = i + gap;
+                    temp = num[j];
+                    while (j - gap > 0 && temp < num[j - gap])
+                    {
+                        num[j] = num[j - gap];
+                        j = j - gap;
+                    }
+                    num[j] = temp;
+                }
+                gap = gap / 2;
+            }
+        }
+        static void DisplayElements()
+        {
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
